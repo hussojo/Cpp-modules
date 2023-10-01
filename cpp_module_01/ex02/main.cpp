4a164c1ff287f31804f5a00c38c28745a80c4fc2
Josefina Husso <husso.josefina@gmail.com>
2023-10-01 14:03:06 +0300

    ex02 done

---
+++ cpp_module_01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 13:36:03 by jhusso            #+#    #+#             */
+/*   Updated: 2023/10/01 14:01:57 by jhusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string var = "HI THIS IS BRAIN";
+	std::string *stringPTR = &var;
+	std::string &stringREF = var;
+
+	std::cout << "The memory address of the string variable: " << &var << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "The value of the string variable: " << var << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+}
