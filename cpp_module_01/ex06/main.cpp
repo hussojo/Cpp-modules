08e1ba4ad6a4faca7646ee1ec72b9af95d7b3fb7
Josefina Husso <husso.josefina@gmail.com>
2023-10-10 18:05:11 +0300

    module 01 done

--- cpp_module_01/ex06/main.cpp
+++ cpp_module_01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 07:34:18 by jhusso            #+#    #+#             */
-/*   Updated: 2023/10/10 17:51:40 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/10/10 18:04:01 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,26 @@
 int main(void)
 {
 	Harl harl;
+	std::string level;
 
-	harl.complain("DEBUG");
-	harl.complain("INFO");
-	harl.complain("WARNING");
-	harl.complain("ERROR");
+	std::cout << "Which level you want Harl to complain?"
+	std::cout << "Choose a number:" << std::endl;
+	std::cout << "1 debug" << std::endl;
+	std::cout << "2 info" << std::endl;
+	std::cout << "3 warning" << std::endl;
+	std::cout << "4 error" << std::endl;
+	std::cin >> level;
+	int nro = std::stoi(level);
+
+	switch (nro)
+	{
+		case 1:
+			harl.complain("DEBUG");
+		case 2:
+			harl.complain("INFO");
+		case 3:
+			harl.complain("WARNING");
+		case 4:
+			harl.complain("ERROR");
+	}
 }
