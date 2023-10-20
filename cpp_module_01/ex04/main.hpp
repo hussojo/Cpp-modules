ca727928797b41e2633e653368f3592d56ad7673
Josefina Husso <husso.josefina@gmail.com>
2023-10-20 07:53:55 +0300

    module 01 ex04 corrected and retested

--- cpp_module_01/ex04/main.hpp
+++ cpp_module_01/ex04/main.hpp
@@ -6,9 +6,12 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 07:41:52 by jhusso            #+#    #+#             */
-/*   Updated: 2023/10/19 15:10:58 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/10/20 07:52:01 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef MAIN_HPP
+#define MAIN_HPP
 
 #include <iostream>
 #include <fstream>
@@ -21,3 +24,4 @@ fstream to read and write
 
 int	replace_and_copy(std::string filename, std::ifstream& file, std::string s1, std::string s2);
 
+#endif
