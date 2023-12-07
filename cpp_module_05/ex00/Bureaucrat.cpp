cb466b67537f2f7b3b42a85afa40205443ce31db
hussojo <josefina.husso@icloud.com>
2023-12-07 13:59:09 +0200

    module05 ex00 done

---
+++ cpp_module_05/ex00/Bureaucrat.cpp
@@ -0,0 +1,78 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name(""), _grade(0)
+{
+		std::cout << "Bureaucrat (default) created!" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string name, unsigned int grade) : _name(name), _grade(grade)
+{
+
+	if (_grade < 1 )
+		throw Bureaucrat::GradeTooHighException();
+	if (_grade > 150 )
+		throw Bureaucrat::GradeTooLowException();
+	std::cout << "Created bureaucrat called\n\tname: " << _name << "\n\tgrade: " << _grade << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other)
+{
+	*this = other;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+	if (this != &other)
+	{
+		_name = other._name;
+		_grade = other._grade;
+	}
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat destructed!" << std::endl;
+}
+
+void Bureaucrat::incrementGrade()
+{
+	this->_grade--;
+	if (_grade < 1 )
+		throw Bureaucrat::GradeTooHighException();
+	if (_grade > 150 )
+		throw Bureaucrat::GradeTooLowException();
+}
+
+void Bureaucrat::decrementGrade()
+{
+	this->_grade++;
+	if (_grade < 1 )
+		throw Bureaucrat::GradeTooHighException();
+	if (_grade > 150 )
+		throw Bureaucrat::GradeTooLowException();
+}
+
+std::string Bureaucrat::getName() const
+{
+	return _name;
+}
+
+unsigned int Bureaucrat::getGrade() const
+{
+	if (_grade < 1 )
+		throw Bureaucrat::GradeTooHighException();
+	if (_grade > 150 )
+		throw Bureaucrat::GradeTooLowException();
+	return _grade;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	 return "Grade is too high!";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	 return "Grade is too low!";
+}
---
+++ cpp_module_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,34 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+class Bureaucrat {
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string name, unsigned int grade);
+		Bureaucrat(const Bureaucrat &other);
+		Bureaucrat &operator=(const Bureaucrat &other);
+		~Bureaucrat();
+
+		class GradeTooHighException : public std::exception {
+			public: const char* what() const throw(); };
+		class GradeTooLowException : public std::exception {
+			public: const char* what() const throw();	};
+
+		void incrementGrade();
+		void decrementGrade();
+
+		std::string getName() const;
+		unsigned int getGrade() const;
+
+		// increment and decrement grade!
+
+	private:
+		std::string _name;
+		unsigned int _grade;
+} ;
+
+#endif
