802fd1482452ba29d885615b1164369c6e6ca6ca
hussojo <josefina.husso@icloud.com>
2023-12-18 13:38:04 +0200

    rready

---
+++ cpp_module_06/ex01/Serializer.cpp
@@ -0,0 +1,12 @@
+#include "Serializer.hpp"
+
+// It takes a pointer and converts it to the unsigned integer type uintptr_t.
+uintptr_t Serializer::serialize(Data *ptr)
+{
+	return uintptr_t();
+}
+// It takes an unsigned integer parameter and converts it to a pointer to Data.
+Data *Serializer::deserialize(uintptr_t raw)
+{
+	return nullptr;
+}
