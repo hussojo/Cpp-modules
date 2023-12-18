93b9489461d06aa6811457a60c8c49194941e967
hussojo <josefina.husso@icloud.com>
2023-12-18 14:28:54 +0200

    ex01 done

--- cpp_module_06/ex01/main.cpp
+++ cpp_module_06/ex01/main.cpp
@@ -1,8 +1,18 @@
 #include "Serializer.hpp"
 
+uintptr_t Serializer::serialize(Data *ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data *Serializer::deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data *>(raw);
+}
+
 int main(void)
 {
-	Data num;
+	Data num; // creating a data object
 	num.val = 27;
 	uintptr_t serialize = Serializer::serialize(&num);
 	std::cout << &serialize << " Serialized" <<std::endl;
