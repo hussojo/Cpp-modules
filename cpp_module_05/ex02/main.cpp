8e1336b47a3d78db54acd0e3de3f081530357b71
hussojo <josefina.husso@icloud.com>
2023-12-19 11:31:13 +0200

    done

--- cpp_module_05/ex02/main.cpp
+++ cpp_module_05/ex02/main.cpp
@@ -3,47 +3,6 @@
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
 
-// int main()
-// {
-// 	try
-// 	{
-// 		Bureaucrat masa("Masa", 30);
-// 		std::cout << masa << std::endl;
-// 		Bureaucrat tepa("Tepa", 3);
-// 		std::cout << tepa << std::endl;
-// 		Bureaucrat menni("Menni", 8);
-// 		std::cout << menni << std::endl;
-
-
-// 		ShrubberyCreationForm scf("home");
-// 		std::cout << scf << std::endl;
-
-// 		ShrubberyCreationForm scf2("home");
-// 		std::cout << scf << std::endl;
-
-// 		PresidentialPardonForm ppf("Teemu");
-// 		std::cout << ppf << std::endl;
-
-// 		RobotomyRequestForm rrf("Karen");
-// 		std::cout << rrf << std::endl;
-
-// 		masa.signForm(scf);
-// 		masa.executeForm(scf);
-// 		masa.executeForm(scf2);
-// 		std::cout << "\n";
-// 		tepa.signForm(ppf);
-// 		tepa.executeForm(ppf);
-// 		std::cout << "\n";
-// 		menni.signForm(rrf);
-// 		menni.executeForm(rrf);
-// 	}
-// 	catch (const std::exception& e) {
-// 		std::cerr << e.what() << std::endl;
-// 	}
-// 	return 0;
-// }
-
-
 int main()
 {
 
