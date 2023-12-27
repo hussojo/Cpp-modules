8c1fc3aca1831c378b5b263a67e88fa1ae790f7c
hussojo <josefina.husso@icloud.com>
2023-12-27 13:21:41 +0200

    ex00 modified main

--- cpp_module_07/ex00/Templates.hpp
+++ cpp_module_07/ex00/Templates.hpp
@@ -7,28 +7,28 @@
 template <typename T>
 void swap(T &a, T &b)
 {
-    T temp;
-    temp = a;
-    a = b;
-    b = temp; 
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
 }
 
 template <typename T>
 T min(T a, T b)
 {
-    if (a < b)
-        return a;
-    else
-        return b;
+	if (a < b)
+		return a;
+	else
+		return b;
 }
 
 template <typename T>
 T max(T a, T b)
-{
-    if (a > b)
-        return a;
-    else
-        return b;
+	{
+	if (a > b)
+		return a;
+	else
+		return b;
 }
 
 #endif
--- cpp_module_07/ex00/main.cpp
+++ cpp_module_07/ex00/main.cpp
@@ -1,59 +1,41 @@
 #include "Templates.hpp"
 
 int main( void ) {
+std::cout << "---------- INT TESTS ----------" << std::endl;
 int a = 2;
 int b = 3;
+std::cout << "before swap:\ta = " << a << ", b = " << b << std::endl;
 ::swap( a, b );
-std::cout << "a = " << a << ", b = " << b << std::endl;
+std::cout << "after swap:\ta = " << a << ", b = " << b << std::endl;
 std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+std::cout << "---------- DOUBLE TESTS ----------" << std::endl;
+double e = 42.2;
+double f = 4.3;
+std::cout << "before swap:\te = " << e << ", f = " << f << std::endl;
+::swap( e, f );
+std::cout << "after swap:\te = " << e << ", f = " << f << std::endl;
+std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+
+std::cout << "---------- FLOAT TESTS ----------" << std::endl;
+double g = 15.15f;
+double h = 4.8f;
+std::cout << "before swap:\tg = " << g << ", h = " << h << std::endl;
+::swap( g, h );
+std::cout << "after swap:\tg = " << g << ", h = " << h << std::endl;
+std::cout << "min( g, h ) = " << ::min( g, h ) << std::endl;
+std::cout << "max( g, h ) = " << ::max( g, h ) << std::endl;
+
+std::cout << "---------- CHAR TESTS ----------" << std::endl;
 std::string c = "chaine1";
 std::string d = "chaine2";
+std::cout << "before swap:\tc = " << c << ", d = " << d << std::endl;
 ::swap(c, d);
-std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "after swap:\tc = " << c << ", d = " << d << std::endl;
 std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 return 0;
 }
 
-/*
-int	main(void)
-{
-	int			a;
-	int			b;
-	char		c;
-	char		d;
-	float		e;
-	float		f;
-
-	a = 1;
-	b = 42;
-	c = 'a';
-	d = 'z';
-	e = 3.14;
-	f = 6.28;
-
-	std::cout << "The values of the integers a and b are " << a << " and " << b << " respectively." << std::endl;
-	::swap<int>(a, b);
-	std::cout << "The values of the integers a and b after swaping them are " << a << " and " << b << " respectively." << std::endl << std::endl;
-
-	std::cout << "The characters c and d are " << c << " and " << d << " respectively." << std::endl;
-	::swap<char>(c, d);
-	std::cout << "The characters c and d after swaping them are " << c << " and " << d << " respectively." << std::endl << std::endl;
-
-	std::cout << "The values of the floating-point nunbers e and f are " << e << " and " << f << " respectively." << std::endl;
-	::swap<float>(e, f);
-	std::cout << "The values of the floating-point numbers e and f after swaping them are " << e << " and " << f << " respectively." << std::endl << std::endl;
-
-	std::cout << "The max value between the integers a and b is " << ::max<int>(a, b) << std::endl;
-	std::cout << "The min value between the integers a and b is " << ::min<int>(a, b) << std::endl << std::endl;
-
-	std::cout << "The max value between the characters c and d is " << ::max<char>(c, d) << std::endl;
-	std::cout << "The min value between the characters c and d is " << ::min<char>(c, d) << std::endl << std::endl;
-
-	std::cout << "The max value between the floating-point numbers e and f is " << ::max<float>(e, f) << std::endl;
-	std::cout << "The min value between the floating-point numbers e and f is " << ::min<float>(e, f) << std::endl << std::endl;
-
-	return (0);
-}
-*/
