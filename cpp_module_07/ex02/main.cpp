9957628520e7a752be6f834cc49811b6168f7498
hussojo <josefina.husso@icloud.com>
2023-12-27 14:18:57 +0200

    added main

--- cpp_module_07/ex02/main.cpp
+++ cpp_module_07/ex02/main.cpp
@@ -1,22 +1,52 @@
 #include "Array.hpp"
 
-int main( void )
+#define MAX_VAL 15
+
+int main(int, char**)
 {
-    try
-    {
-        // unsigned int size = 5;
-        Array<int> array1;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    
+
+	std::cout << "-------- creating array of specific size ---------" << std::endl;
+	Array<int> numbers(MAX_VAL);
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand() % 50;
+		numbers[i] = value;
+	}
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		std::cout << "numbers[" << i << "]: " << numbers[i] << std::endl;
+	}
+	std::cout << "-------- testing copy assigment constructor---------" << std::endl;
+	Array<int> mirror = numbers;
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		std::cout << "mirror[" << i << "]: " << mirror[i] << std::endl;
+	}
+	std::cout << "-------- testing copy constructor---------" << std::endl;
+	Array<int> table(numbers);
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		std::cout << "table[" << i << "]: " << table[i] << std::endl;
+	}
+	try
+	{
+		std::cout << "-------- creating empty array ---------" << std::endl;
+		Array<char> letters;
+		std::cout << letters[0] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::cout << "-------- trying to access out of bounds ---------" << std::endl;
+		std::cout << numbers[15] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	return 0;
 }
-
-
-// Tip: Try to compile int * a = new int(); then display *a.
-// creating a pointer to integer and displaying value: value-initialized to zero
-// -> what should happen in parametrisized constructor
-
-// test deep copy
