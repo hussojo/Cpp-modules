23737062d7468d37c8bea0bbfc9a09a60189e195
jhusso <josefina.husso@icloud.com>
2023-12-31 13:31:46 +0200

    first version of ex00 done

--- cpp_module_08/ex00/EasyFind.hpp
+++ cpp_module_08/ex00/EasyFind.hpp
@@ -3,12 +3,18 @@
 
 #include <algorithm>
 #include <exception>
+#include <vector>
+#include <iostream>
+//#include <string>
+
 
 template <typename T>
 typename T::iterator easyfind(T &a, int b)
 {
-	if (std::find(a.beging(), a.end(), b) == a.end())
-		throw ;
+	typename T::iterator it = std::find(a.begin(), a.end(), b);
+	if (it == a.end())
+		throw std::runtime_error("Value not found in the given container.");
+	return it;
 }
 
 /*
--- cpp_module_08/ex00/main.cpp
+++ cpp_module_08/ex00/main.cpp
@@ -2,5 +2,20 @@
 
 int main (void)
 {
-
+	try
+	{
+		std::vector<int> myVec;
+		myVec.push_back(1);
+		myVec.push_back(2);
+		myVec.push_back(3);
+		myVec.push_back(4);
+		myVec.push_back(5);
+		myVec.push_back(6);
+		easyfind(myVec, 1);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
 }
