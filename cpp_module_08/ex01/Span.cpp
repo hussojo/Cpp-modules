9176615261065bdd49e39067c3c4e4a3b3ecdfad
jhusso <josefina.husso@icloud.com>
2023-12-31 14:56:38 +0200

    last func missing, write main

--- cpp_module_08/ex01/Span.cpp
+++ cpp_module_08/ex01/Span.cpp
@@ -1,1 +1,41 @@
 #include "Span.hpp"
+
+Span::Span(unsigned int n) : _size(n) {}
+
+Span::Span(const Span &other) : _size(other._size) {}
+
+Span &Span::operator=(const Span &other)
+{
+	_size = other._size;
+	return *this;
+}
+
+Span::~Span() {}
+
+void Span::addNumber(int i)
+{
+	if (myVec.size() >= _size)
+		throw std::runtime_error("Span is full!");
+	myVec.push_back(i);
+}
+
+unsigned int Span::shortestSpan()
+{
+	if (myVec.size() < 2)
+		throw std::invalid_argument("Not enough elements in Span (one or less)");
+	std::sort(myVec.begin(), myVec.end());
+	int shortest = UINT8_MAX;
+	for (unsigned int i = 1; i < this->_size; i++)
+		shortest = std::min(shortest, myVec[i] - myVec[i - 1]);
+	return shortest;
+}
+
+unsigned int Span::longestSpan()
+{
+	if (myVec.size() < 2)
+		throw std::invalid_argument("Not enough elements in Span (one or less)");
+	std::sort(myVec.begin(), myVec.end());
+	return myVec.end() - myVec.begin();
+}
+
+Span::Span() : _size(0) {}
--- cpp_module_08/ex01/main.cpp
+++ cpp_module_08/ex01/main.cpp
@@ -1,1 +1,14 @@
 #include "Span.hpp"
+
+int main(void)
+{
+	try
+	{
+		/* code */
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+}
