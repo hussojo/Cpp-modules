9399411224c4539acae212e4ea84fa09f28c26d4
hussojo <josefina.husso@icloud.com>
2024-01-08 08:55:28 +0200

    ex00 check, better main

--- cpp_module_08/ex00/EasyFind.hpp
+++ cpp_module_08/ex00/EasyFind.hpp
@@ -4,9 +4,9 @@
 #include <algorithm>
 #include <exception>
 #include <vector>
+#include <list>
+#include <deque>
 #include <iostream>
-//#include <string>
-
 
 template <typename T>
 typename T::iterator easyfind(T &a, int b)
@@ -16,16 +16,5 @@ typename T::iterator easyfind(T &a, int b)
 		throw std::runtime_error("Value not found in the given container.");
 	return it;
 }
-
-/*
-Write a function template easyfind that accepts a type T. It takes two parameters.
-The first one has type T and the second one is an integer.
-
-Assuming T is a container of integers, this function has to find the first occurrence
-of the second parameter in the first parameter.
-
-If no occurrence is found, you can either throw an exception or return an error value
-of your choice. If you need some inspiration, analyze how standard containers behave
-*/
 
 #endif
--- cpp_module_08/ex00/main.cpp
+++ cpp_module_08/ex00/main.cpp
@@ -1,21 +1,52 @@
 #include "EasyFind.hpp"
 
-int main (void)
-{
-	try
-	{
-		std::vector<int> myVec;
-		myVec.push_back(1);
-		myVec.push_back(2);
-		myVec.push_back(3);
-		myVec.push_back(4);
-		myVec.push_back(5);
-		myVec.push_back(6);
-		easyfind(myVec, 1);
+int main(void) {
+	std::cout << "----------VECTOR----------" << std::endl;
+	std::vector<int> vec;
+	for (int i = 0; i < 1000; i++) {
+		vec.push_back(i);
 	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
+	try {
+		easyfind(vec, 42);
+		easyfind(vec, 172);
+		easyfind(vec, 2345);
+		easyfind(vec, -42);
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
 	}
-	
+
+	std::cout << std::endl << "----------DEQUE----------" << std::endl;
+	std::deque<int> deq;
+	for (int i = 0; i < 1000; i++) {
+		if (i % 2 == 0)
+			deq.push_back(i);
+		else
+			deq.push_front(i);
+	}
+	try {
+		easyfind(deq, 42);
+		easyfind(deq, 172);
+		easyfind(deq, 2345);
+		easyfind(deq, -1);
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << std::endl << "----------LIST----------" << std::endl;
+	std::list<int> li;
+	for (int i = 0; i < 1000; i++) {
+		if (i % 3 == 0)
+			li.insert(li.end(), i);
+		else
+			li.insert(li.begin(), i);
+	}
+	try {
+		easyfind(li, 42);
+		easyfind(li, 172);
+		easyfind(li, 2345);
+		easyfind(li, -1);
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	return (0);
 }
