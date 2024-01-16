7b26ed984672db7a6609f133082729222ca44f6e
hussojo <josefina.husso@icloud.com>
2024-01-16 14:18:24 +0200

    starting with vector

--- cpp_module_09/ex02/PmergeMe.cpp
+++ cpp_module_09/ex02/PmergeMe.cpp
@@ -1,1 +1,18 @@
 #include "PmergeMe.hpp"
+
+void sortAndPrint(char ** input)
+{
+	// for (int i = 0; input[i]; i++)
+	// 	std::cout << input[i] << std::endl;
+	{ //deque
+		// std::deque <unsigned int> deque;
+		// deque.sort();
+		// deque.print();
+	}
+	{ //vector
+		std::vector <unsigned int> vector;
+		vector.sort();
+		vector.print();
+
+	}
+}
--- cpp_module_09/ex02/PmergeMe.hpp
+++ cpp_module_09/ex02/PmergeMe.hpp
@@ -1,8 +1,16 @@
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <exception>
+#include <algorithm>
+
+void sortAndPrint(char ** input);
+
 /*
-- two containers, array and vector
+- two containers, deque and vector
 - templated sorting algorithm
 */
 
