34ca126d4d3b49ba0d355955eabda4591e36cd0c
hussojo <josefina.husso@icloud.com>
2024-01-22 09:32:23 +0200

    mod09 ex01 done

--- cpp_module_09/ex01/main.cpp
+++ cpp_module_09/ex01/main.cpp
@@ -3,7 +3,10 @@
 int main(int ac, char **av)
 {
 	if (ac != 2)
+	{
 		std::cerr << "Usage: ./RPN \"inverted Polish mathematical expression\"" << std::endl;
+		return (1);
+	}
 	try
 	{
 		printRpn(av[1]);
